2794bde8ceab1ab007ae5ff388cc7532a538b43b
Ezequiel-cheque <delaluzezequiel@gmail.com>
2022-08-05 23:58:39 -0500

    finish react integartion

---
+++ src/pages/Home.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+import homeImage from "../assets/images/home.png";
+import uaemLogo from "../assets/images/uaem.png";
+import cincco from "../assets/images/cincco.png";
+import posgrado from "../assets/images/posgrado.png";
+import login from "../assets/images/login.png";
+import loginIcon from "../assets/images/loginIcon.png";
+
+
+export default function Home() {
+
+    const [isLogin, setIsLogin] = useState(false);
+
+    const handleLogin = () => {
+      setIsLogin(!isLogin);
+    }; 
+
+    return(
+        <>
+          <div className="home">
+            
+            <div className="home__left-container">
+              <div className="home__left-container-image">
+                {
+                  isLogin ? (
+                    <img src={login} alt="logo"/>
+                  ) : (
+                    <img src={homeImage} alt="logo"/>
+                  )
+                }
+              </div>
+            </div>
+
+            <div className="home__right-container">
+              <div className="home__right-container-image">
+                <img src={uaemLogo} alt="logo"/>
+              </div>
+              
+              {
+                isLogin ? (
+                  <div className="home__right-container-formLogin">
+                    <div className="home__right-container-formLogin-image">
+                      <img src={loginIcon} alt="logo"/>
+                    </div>
+                    <input type="text" placeholder="Ingresa tu clave"/>
+                    <button>Entrar</button>
+                    <p onClick={handleLogin}>INGRESAR AL INICIO</p>
+                  </div>
+                ) : (
+                  <div className="home__right-container-form">
+                    <input type="text" placeholder="Ingresa tu ID"/>
+                    <button>Iniciar</button>
+                    <p onClick={handleLogin}>INGRESAR AL PANEL</p>
+                  </div>
+                )
+              }
+
+              <div className="home__right-container-banner">
+                <div>
+                  <img src={cincco} alt="logo"/>
+                </div>
+                <div>
+                  <img src={posgrado} alt="logo"/>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </>
+    ); 
+}

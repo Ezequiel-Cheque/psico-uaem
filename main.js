c568778daf4e1eb559da86711728636b9eb8becd
Ezequiel-cheque <delaluzezequiel@gmail.com>
2022-08-05 23:20:42 -0500

    firs commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const { win, BrowserWindow, app } = require("electron");
+// const path = require('path');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            worldSafeExecuteJavascript: true,
+            contextIsolation: true
+        }
+    })
+
+    win.loadFile('index.html');
+}
+
+// require('electron-reload')(__dirname, {
+//     electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+// })
+
+app.whenReady().then(createWindow);

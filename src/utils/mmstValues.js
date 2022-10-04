d4a8972523f1660044256186c476db1c1bc8663f
Ezequiel-Cheque <delaluzezequiel@gmail.com>
2022-10-03 23:36:38 -0500

    Fix mmst size and initial images

--- src/utils/mmstValues.js
+++ src/utils/mmstValues.js
@@ -15,39 +15,19 @@ import positivas10 from "../assets/images/positivas/2260.jpg";
 import positivas11 from "../assets/images/positivas/2655.jpg";
 import positivas12 from "../assets/images/positivas/2660.jpg";
 
-import neutras0 from "../assets/images/neutras/2190.jpg";
-import neutras1 from "../assets/images/neutras/5001.jpg";
-import neutras2 from "../assets/images/neutras/5300.jpg";
-import neutras3 from "../assets/images/neutras/7004.jpg";
-import neutras4 from "../assets/images/neutras/7006.jpg";
-import neutras5 from "../assets/images/neutras/7009.jpg";
-import neutras6 from "../assets/images/neutras/7010.jpg";
-import neutras7 from "../assets/images/neutras/7020.jpg";
-import neutras8 from "../assets/images/neutras/7030.jpg";
-import neutras9 from "../assets/images/neutras/7034.jpg";
-import neutras10 from "../assets/images/neutras/7035.jpg";
-import neutras11 from "../assets/images/neutras/7040.jpg";
-import neutras12 from "../assets/images/neutras/7050.jpg";
-import neutras13 from "../assets/images/neutras/7080.jpg";
-import neutras14 from "../assets/images/neutras/7090.jpg";
-import neutras15 from "../assets/images/neutras/7095.jpg";
-import neutras16 from "../assets/images/neutras/7100.jpg";
-import neutras17 from "../assets/images/neutras/7130.jpg";
-import neutras18 from "../assets/images/neutras/7140.jpg";
-import neutras19 from "../assets/images/neutras/7150.jpg";
-import neutras20 from "../assets/images/neutras/7170.jpg";
-import neutras21 from "../assets/images/neutras/7175.jpg";
-import neutras22 from "../assets/images/neutras/7190.jpg";
-import neutras23 from "../assets/images/neutras/7200.jpg";
-import neutras24 from "../assets/images/neutras/7211.jpg";
-import neutras25 from "../assets/images/neutras/7217.jpg";
-import neutras26 from "../assets/images/neutras/7224.jpg";
-import neutras27 from "../assets/images/neutras/7233.jpg";
-import neutras28 from "../assets/images/neutras/7235.jpg";
-import neutras29 from "../assets/images/neutras/7450.jpg";
-import neutras30 from "../assets/images/neutras/7510.jpg";
-import neutras31 from "../assets/images/neutras/7545.jpg";
-import neutras32 from "../assets/images/neutras/7580.jpg";
+import neutras0 from "../assets/images/neutras/2057.jpg";
+import neutras1 from "../assets/images/neutras/3181.jpg";
+import neutras2 from "../assets/images/neutras/6550.jpg";
+import neutras3 from "../assets/images/neutras/3530.jpg";
+import neutras4 from "../assets/images/neutras/2057.jpg";
+import neutras5 from "../assets/images/neutras/3550.jpg";
+import neutras6 from "../assets/images/neutras/9040.jpg";
+import neutras7 from "../assets/images/neutras/6560.jpg";
+import neutras8 from "../assets/images/neutras/3301.jpg";
+import neutras9 from "../assets/images/neutras/1440.jpg";
+import neutras10 from "../assets/images/neutras/6560.jpg";
+import neutras11 from "../assets/images/neutras/8230.jpg";
+import neutras12 from "../assets/images/neutras/3550.jpg";
 
 import negativas1 from "../assets/images/negativas/1301.jpg";
 import negativas0 from "../assets/images/negativas/1300.jpg";
@@ -172,27 +152,7 @@ const neutras = [
     neutras9,
     neutras10,
     neutras11,
-    neutras12,
-    neutras13,
-    neutras14,
-    neutras15,
-    neutras16,
-    neutras17,
-    neutras18,
-    neutras19,
-    neutras20,
-    neutras21,
-    neutras22,
-    neutras23,
-    neutras24,
-    neutras25,
-    neutras26,
-    neutras27,
-    neutras28,
-    neutras29,
-    neutras30,
-    neutras31,
-    neutras32
+    neutras12
 ];
 
 const getAleatoryImages = (num, type) => {
@@ -216,7 +176,7 @@ const getAleatoryImages = (num, type) => {
     return newImages;
 };
 
-const images1 = getAleatoryImages(5, "neutras").map((item, index) => (
+const images1 = neutras.map((item, index) => (
     {
         id: index,
         img: item,
